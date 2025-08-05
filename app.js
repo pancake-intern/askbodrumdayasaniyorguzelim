afe7b4df94779f042c7c207a3376c61e88ea7801
Muhammed Talha Özdemir <183485197+pancake-intern@users.noreply.github.com>
2025-08-05 10:20:48 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+//Yorum satırı
+
+// * Enes
+//?Amogus
+//! Dikkat
+// TODO: yap
+// console.log("talha");
+// document.writeln("ELLO WORLDIE.");
+// console.log(7-6);
+// let a=5;
+// let b=12;
+// alert("toplamları:"+ (a+b));
+console.log(window);
+
+//mahmut bak şurada şöyle yaprım buraya dikkat et.
+
+// TODO:: FONKSİYON İÇİNDE TANIMLI DEĞİŞKENLER DISARIDAN ERİŞİLEMEZ. function scope.
+// ! if içinde vs ise  block scope oluyor.
+// let a=10;
+// a=4;
+// while(a<10){
+//     console.log(a);
+//     a++;
+
+// }
+//! var artık kullanılmıyor. let ve const yerine gelmiş.
+
+//? CONST OBJENİN DEĞİŞKEN DEĞERLERİ DEGİSEBİLİR. 
+
+// const user ={
+//     username: "talha",
+//     password: "123"
+
+// }
+// console.log(user);
+
+// user.password ="talhaozdemir";
+// console.log(user);
+
+
+// = ATAMA. 
+// ? == ESITMI DİYE KONTROL . FARKLI VERİ TİPİ OLSA BİLE
+// TODO:: === HERSEYI ESİT Mİ DİYE.
+
+//! % KALANI VERİYOR.
+
+console.log(2**3);
+console.log(13%5);
+
+// ? == === filan
+
+/*  
+
+? && VE
+!  DEGIL
+|| VEYA
+
+
+*/
+
+
+// alert("FREERAM DOWNLOADING... NOW...");
+// let isim =prompt("İsminizi giriniz:");
+// alert("WE HAVE YOUR ADDRESS "+ isim);
+
+soru = prompt("Bir soru sorunuz:");
+let cevaplar = ["Evet", "Hayır"];
+let rastgele = Math.floor(Math.random() * cevaplar.length);
+alert("Cevap: " + cevaplar[rastgele]);

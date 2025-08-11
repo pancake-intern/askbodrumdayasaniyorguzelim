9379530dda6632ef65987c0bf116c746895bc5c1
Muhammed Talha Özdemir <183485197+pancake-intern@users.noreply.github.com>
2025-08-11 10:46:07 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,230 @@
+//input.value=''; yapıcan .
+let counter=1;
+let plannedTasks= JSON.parse(localStorage.getItem('plannedTasks')) || [];
+let doingTasks= JSON.parse(localStorage.getItem('doingTasks')) || [];
+let doneTasks= JSON.parse(localStorage.getItem('doneTasks')) || [];
+
+
+
+// input=document.getElementById("input");
+// console.log(input.value);
+
+window.addEventListener("load", function() {
+    plannedTasks.forEach(task => addPlannedFromStorage(task));
+    doingTasks.forEach(task => addDoingFromStorage(task));
+    doneTasks.forEach(task => addDoneFromStorage(task));
+});
+
+document.querySelector("#inputButton").addEventListener("click",function(e){
+    e.preventDefault();
+    input=document.getElementById("input");
+    console.log(input.value);
+    addPlanned(input.value);
+    document.getElementById("input").value='';
+});
+
+function addPlanned(task){
+    if(task.length<1) return;
+    plannedTasks.push(task);
+    localStorage.setItem('plannedTasks', JSON.stringify(plannedTasks));
+    
+    const newLi = document.createElement("li");
+    newLi.classList.add("mb-2", "p-2", "bg-dark","text-light", "rounded", "d-flex", "justify-content-between", "align-items-center");
+    newLi.id = `plannedButton${counter}`;
+    
+    const taskText = document.createElement("span");
+    taskText.textContent = task;
+    
+    const tickButton = document.createElement("button");
+    tickButton.innerHTML = "✓";
+    tickButton.classList.add("btn", "btn-success", "btn-sm");
+    
+    tickButton.addEventListener("click", function(e) {
+        console.log(`Yapılanlara taşınıyor: ${task}`);
+        const index = plannedTasks.indexOf(task);
+        plannedTasks.splice(index, 1);
+        localStorage.setItem('plannedTasks', JSON.stringify(plannedTasks));
+        addDoing(task);
+        newLi.remove();
+    });
+    
+    newLi.appendChild(taskText);
+    newLi.appendChild(tickButton);
+    
+    const plannedList = document.getElementById("planned-ul");
+    plannedList.appendChild(newLi);
+    counter++;
+}
+function addDoing(task){
+    if(task.length<1) return;
+    doingTasks.push(task);
+    localStorage.setItem('doingTasks', JSON.stringify(doingTasks));
+    
+    const newLi = document.createElement("li");
+    newLi.classList.add("mb-2", "p-2", "bg-dark","text-light", "rounded", "d-flex", "justify-content-between", "align-items-center");
+    newLi.id = `doingButton${counter}`;
+    
+    const taskText = document.createElement("span");
+    taskText.textContent = task;
+
+    const backButton = document.createElement("button");
+    backButton.innerHTML = "←";
+    backButton.classList.add("btn", "btn-warning", "btn-sm");
+    
+    const tickButton = document.createElement("button");
+    tickButton.innerHTML = "✓";
+    tickButton.classList.add("btn", "btn-success", "btn-sm");
+    
+    tickButton.addEventListener("click", function(e) {
+        console.log(`Bitmiş olanlara taşınıyor: ${task}`);
+        const index = doingTasks.indexOf(task);
+        doingTasks.splice(index, 1);
+        localStorage.setItem('doingTasks', JSON.stringify(doingTasks));
+        addDone(task);
+        newLi.remove();
+    });
+
+    backButton.addEventListener("click",function(e){
+        const index= doingTasks.indexOf(task);
+        doingTasks.splice(index,1);
+        localStorage.setItem('doingTasks',JSON.stringify(doingTasks));
+        addPlanned(task);
+        newLi.remove();
+    });
+
+    newLi.appendChild(backButton);
+    newLi.appendChild(taskText);
+    newLi.appendChild(tickButton);
+    
+    const doingList = document.getElementById("doing-ul");
+    doingList.appendChild(newLi);
+    counter++;
+}
+function addDone(task){
+    if(task.length<1) return;
+    doneTasks.push(task);
+    localStorage.setItem('doneTasks', JSON.stringify(doneTasks));
+    
+    const newLi = document.createElement("li");
+    newLi.classList.add("mb-2", "p-2", "bg-success","text-light", "rounded", "d-flex", "justify-content-between", "align-items-center");
+    newLi.id = `doneButton${counter}`;
+    
+    const backButton = document.createElement("button");
+    backButton.innerHTML = "←";
+    backButton.classList.add("btn", "btn-warning", "btn-sm");
+    
+    const taskText = document.createElement("span");
+    taskText.textContent = task;
+    
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "✗ ";
+    deleteButton.classList.add("btn", "btn-danger", "btn-sm");
+    
+    deleteButton.addEventListener("click", function(e) {
+        const index = doneTasks.indexOf(task);
+        doneTasks.splice(index, 1);
+        localStorage.setItem('doneTasks', JSON.stringify(doneTasks));
+        newLi.remove();
+    });
+    backButton.addEventListener("click",function(e){
+        const index= doneTasks.indexOf(task);
+        doneTasks.splice(index,1);
+        localStorage.setItem('doneTasks',JSON.stringify(doneTasks));
+        addDoing(task);
+        newLi.remove();
+    });
+    newLi.appendChild(backButton);
+    newLi.appendChild(taskText);
+    newLi.appendChild(deleteButton);
+    
+    
+    const doneList = document.getElementById("done-ul");
+    doneList.appendChild(newLi);
+    counter++;
+}
+
+function addPlannedFromStorage(task){
+    const newLi = document.createElement("li");
+    newLi.classList.add("mb-2", "p-2", "bg-dark","text-light", "rounded", "d-flex", "justify-content-between", "align-items-center");
+    newLi.id = `plannedButton${counter}`;
+    
+    const taskText = document.createElement("span");
+    taskText.textContent = task;
+    
+    const tickButton = document.createElement("button");
+    tickButton.innerHTML = "✓";
+    tickButton.classList.add("btn", "btn-success", "btn-sm");
+    
+    tickButton.addEventListener("click", function(e) {
+        const index = plannedTasks.indexOf(task);
+        plannedTasks.splice(index, 1);
+        localStorage.setItem('plannedTasks', JSON.stringify(plannedTasks));
+        addDoing(task);
+        newLi.remove();
+    });
+    
+    newLi.appendChild(taskText);
+    newLi.appendChild(tickButton);
+    
+    const plannedList = document.getElementById("planned-ul");
+    plannedList.appendChild(newLi);
+    counter++;
+}
+
+function addDoingFromStorage(task){
+    const newLi = document.createElement("li");
+    newLi.classList.add("mb-2", "p-2", "bg-dark","text-light", "rounded", "d-flex", "justify-content-between", "align-items-center");
+    newLi.id = `doingButton${counter}`;
+    
+    const taskText = document.createElement("span");
+    taskText.textContent = task;
+    
+    const tickButton = document.createElement("button");
+    tickButton.innerHTML = "✓";
+    tickButton.classList.add("btn", "btn-success", "btn-sm");
+    
+    tickButton.addEventListener("click", function(e) {
+        const index = doingTasks.indexOf(task);
+        doingTasks.splice(index, 1);
+        localStorage.setItem('doingTasks', JSON.stringify(doingTasks));
+        addDone(task);
+        newLi.remove();
+    });
+    
+    newLi.appendChild(taskText);
+    newLi.appendChild(tickButton);
+    
+    const doingList = document.getElementById("doing-ul");
+    doingList.appendChild(newLi);
+    counter++;
+}
+
+function addDoneFromStorage(task){
+    const newLi = document.createElement("li");
+    newLi.classList.add("mb-2", "p-2", "bg-success","text-light", "rounded", "d-flex", "justify-content-between", "align-items-center");
+    newLi.id = `doneButton${counter}`;
+    
+    const taskText = document.createElement("span");
+    taskText.textContent = task;
+    
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "✗";
+    deleteButton.classList.add("btn", "btn-danger", "btn-sm");
+
+    
+    
+    deleteButton.addEventListener("click", function(e) {
+        const index = doneTasks.indexOf(task);
+        doneTasks.splice(index, 1);
+        localStorage.setItem('doneTasks', JSON.stringify(doneTasks));
+        newLi.remove();
+    });
+    
+    newLi.appendChild(taskText);
+    newLi.appendChild(deleteButton);
+    
+    const doneList = document.getElementById("done-ul");
+    doneList.appendChild(newLi);
+    counter++;
+}
+
